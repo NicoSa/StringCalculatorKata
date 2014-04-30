b224f97f5f3eb807896a0ede90d2485bc9a07b6f
Nico Saueressig <nico@nicosaueressig.de>
2014-05-01 00:22:08 +0100

    got up to four

--- ruby/lib/string_calculator.rb
+++ ruby/lib/string_calculator.rb
@@ -1,3 +1,9 @@
 module StringCalculator
 
+	def self.add(numbers)
+		total = 0
+		numbers.gsub!("\n",",")
+		numbers.split(",").each{|number| total += number.to_i}
+		total
+	end
 end
--- ruby/spec/string_calculator_spec.rb
+++ ruby/spec/string_calculator_spec.rb
@@ -4,7 +4,21 @@ require "string_calculator"
 describe StringCalculator do
 
   it "returns 0 when given an empty string" do
-    true.should == false
+    expect(StringCalculator.add("")).to eq(0)
   end
 
+  it "returns 1 when given 1" do
+  	expect(StringCalculator.add("1")).to eq(1)
+  end
+
+  it "can handle more than 2 integers" do
+  	expect(StringCalculator.add("1,2")).to eq(3)
+  	expect(StringCalculator.add("5,5,10")).to eq(20)
+  end
+
+  it "can handle new lines in strings" do
+  	expect(StringCalculator.add("1\n2,3")).to eq(6)
+  end
+
+  it "raises an exception if the string has a minus sign"
 end
